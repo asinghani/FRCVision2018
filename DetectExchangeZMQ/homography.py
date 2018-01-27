b52d2d51b300f6a81786405f8f67d707d108942a
asinghani <asinghani@users.noreply.github.com>
2018-01-26 22:38:11 -0800

    update

--- DetectExchangeZMQ/homography.py
+++ DetectExchangeZMQ/homography.py
@@ -4,7 +4,7 @@ import time, math
 import zed
 
 img1 = cv2.imread('data/real_target2.png',0)
-cap = zed.ZEDWrapper(framerate = 10)
+cap = zed.ZEDWrapper(framerate = 30)
 
 cameraMatrix = np.array([np.array([1.15881320e+03, 0.00000000e+00, 6.34093244e+02]),
                          np.array([0.00000000e+00, 1.16120116e+03, 3.43339363e+02]),
